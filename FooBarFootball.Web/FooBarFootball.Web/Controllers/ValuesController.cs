b2fd46cb151e645702b993c4b8542442742df16d
Liam Molloy <lmolloy@ancestry.com>
2013-09-16 15:29:43 +0100

    Fixed broken unit tests

--- FooBarFootball.Web/FooBarFootball.Web/Controllers/ValuesController.cs
+++ FooBarFootball.Web/FooBarFootball.Web/Controllers/ValuesController.cs
@@ -22,26 +22,5 @@ namespace FooBarFootball.Web.Controllers
             vm.Cards.Add(card2);
             return Request.CreateResponse(HttpStatusCode.OK, vm);
         }
-
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
     }
 }
