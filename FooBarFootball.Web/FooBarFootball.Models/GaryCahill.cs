99198711b150418eaf77a7bc14a3c11feabcabc5
Liam Molloy <lmolloy@ancestry.com>
2013-09-16 15:45:42 +0100

    added generic gary cahill defender

---
+++ FooBarFootball.Web/FooBarFootball.Models/GaryCahill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBarFootball.Models
+{
+    public class GaryCahill : Card
+    {
+        public GaryCahill()
+        {
+            Id = Guid.NewGuid().ToString();
+            Name = "Gary Cahill";
+            CardType = "Player - Centre Back";
+            BlackCost = 2;
+            Attack = 1;
+            Defence = 3;
+        }
+
+        public override void Play(GameState gameState)
+        {
+            if (!IsTapped && gameState.BlackTactics >= BlackCost)
+            {
+                IsTapped = true;
+            }
+        }
+    }
+}
