6084d0d061a09b9bb876c667314d48e68445ffb3
Liam Molloy <lmolloy@ancestry.com>
2013-10-09 15:20:03 +0100

    Implemented player end point

---
+++ FooBarFootball.Web/FooBarFootball.Web.Tests/XmlPlayerRepositoryTests.cs
@@ -0,0 +1,20 @@
+﻿using FooBarFootball.Data.Implementations;
+using FooBarFootball.Data.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FooBarFootball.Web.Tests
+{
+    [TestClass]
+    public class XmlPlayerRepositoryTests
+    {
+        [TestMethod]
+
+        public void LoadPlayersFromXml()
+        {
+            IPlayerRepository repo = new XmlPlayerRepository("MockXml/players.xml");
+            var cards = repo.Get();
+            Assert.IsNotNull(cards);
+            Assert.IsTrue(cards.Count > 0);
+        }
+    }
+}
---
+++ FooBarFootball.Web/FooBarFootball.Web/Controllers/PlayersController.cs
@@ -0,0 +1,36 @@
+﻿using FooBarFootball.Data.Implementations;
+using FooBarFootball.Data.Interfaces;
+using FooBarFootball.Models;
+using FooBarFootball.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+
+namespace FooBarFootball.Web.Controllers
+{
+    public class PlayersController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            // TODO: Set up DI
+            IPlayerRepository repo = new XmlPlayerRepository(BaseSiteUrl + "Data/players.xml");
+            var cards = repo.Get();
+            return Request.CreateResponse(HttpStatusCode.OK, cards);
+        }
+
+        // TODO: move to utility method somewhere.
+        public static string BaseSiteUrl
+        {
+            get
+            {
+                HttpContext context = HttpContext.Current;
+                string baseUrl = context.Request.Url.Scheme + "://" + context.Request.Url.Authority + context.Request.ApplicationPath.TrimEnd('/') + '/';
+                return baseUrl;
+            }
+        }
+    }
+}
