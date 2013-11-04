49b5fba097ef8c4f4b2ea05d3c2531c6b0f7f697
shaneob <shaneob121@hotmail.com>
2013-11-04 17:22:50 +0000

    added sub menu, separate pages for to display player and move cards

---
+++ FooBarFootball.Web/FooBarFootball.Web/Scripts/app/controllers/moves.js
@@ -0,0 +1,12 @@
+fooBarControllers.controller('MoveCardsController', ['$scope', '$http', function ($scope, $http) {
+    $scope.title = "FooBar Football - Move Cards";
+    $scope.moves = [];
+    $http.get('/api/moves').then(function (data) {
+        var movesArr = [];
+        for (var i = 0; i < data.data.length; i++) {
+            var move = new ModelMove(data.data[i]);
+            movesArr.push(move);
+        }
+        $scope.moves = movesArr;
+    });
+}]);
