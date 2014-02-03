5ccdbe4a93e91d4ec11e3477bac665f9082dbe92
shaneob <shaneob121@hotmail.com>
2014-02-03 12:43:16 +0000

    Created JS unit tests using Karma
    Download VS extension from
    http://visualstudiogallery.msdn.microsoft.com/02f47876-0e7a-4f6c-93f8-1af5d5189225
    to run unit tests in output window

---
+++ FooBarFootball.Web/FooBarFootball.Web/Scripts/UnitTests/unit/controller/home.tests.js
@@ -0,0 +1,20 @@
+﻿describe('Controller: HomeController', function () {
+
+    // load the controller's module
+    beforeEach(module('fooBarControllers'));
+
+    var HomeCtrl,
+      scope;
+
+    // Initialize the controller and a mock scope
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
+        HomeCtrl = $controller('HomeController', {
+            $scope: scope
+        });
+    }));
+
+    it('default unit test', function () {
+        expect(true).toBe(true);
+    });
+});
