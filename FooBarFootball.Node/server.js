7281d42cf42369da434e97955a0c268d778a35d6
Liam Molloy <lmolloy@ancestry.com>
2014-03-21 14:48:23 +0000

    rename queue to listener

--- FooBarFootball.Node/server.js
+++ FooBarFootball.Node/server.js
@@ -1,5 +1,5 @@
 console.log('FooBar Football server starting...');
-var queue = require('./queue_playplayercardfromhandtopitch');
+var queue = require('./listener_queue_playplayercardfromhandtopitch');
 var game_listener = require('./listener_game');
 queue.listen();
 game_listener.listen();
