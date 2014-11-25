fb35bd54eae953b10409442254b4a1081b28b1a5
Liam Molloy <lmolloy@ancestry.com>
2014-11-25 13:13:11 -0800

    new style card attributes plus misc

--- FooBarFootball.Web/Scripts/app/controllers/players.js
+++ FooBarFootball.Web/Scripts/app/controllers/players.js
@@ -3,6 +3,6 @@ fooBarControllers.controller('PlayersCardsController', ['$scope', '$http', '$fir
     $scope.searchTerm = "";
 
     // TODO: why is there a strongly coupled call to new Firebase when we have a $firebase instance passed by DI?
-    var cardsRef = new Firebase("https://foobarfootball.firebaseio.com/Cards");
+    var cardsRef = new Firebase("https://foobarfootball.firebaseio.com/PlayerImport");
     $scope.players = $firebase(cardsRef);
 }]);
